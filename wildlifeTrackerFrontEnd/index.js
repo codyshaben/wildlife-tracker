141bfb0c6089e2b5cd7aae7628c49c4c3142a807
github username <github email address>
2019-10-02 19:42:26 -0600

    created user cards

--- wildlifeTrackerFrontEnd/index.js
+++ wildlifeTrackerFrontEnd/index.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => {
             animalContainer.innerText = ''
             response.animals.forEach(animal => {
-                createCards(animal)
+                createUserCards(animal)
                 
             })
             console.log(response.animals)
@@ -151,14 +151,32 @@ document.addEventListener('DOMContentLoaded', () => {
             document.body.appendChild(animalContainer)
     }
 
-    // function createUserCards(userAnimals){
-    //     const userCard = document.createElement('div')
-    //     const commonName = document.createElement('h1') 
+    function createUserCards(animal){
+        const userCard = document.createElement('div')
+        const commonName = document.createElement('h1') 
+        const scientificName = document.createElement('h4')
+        const category = document.createElement('h5')
+        const description = document.createElement('p')
+        const approachable = document.createElement('p')
+        const status = document.createElement('h6')
+        const image = document.createElement('img')
 
-    //     commonName.innerText = users.animals.common_name
-    //     userCard.appendChild(commonName)
-    //     document.body.appendChild(userCard)
-    // }
+        userCard.className = "user-card"
+
+        commonName.innerText = animal.common_name
+        scientificName.innerText = 'Scientific Name: ' + animal.scientific_name
+        category.innerText = 'Category: ' + animal.category
+        description.innerText = animal.description
+        approachable.innerText = animal.approachable
+        status.innerText = animal.status
+        image.src = animal.image
+
+
+        userCard.appendChild(image)
+        userCard.append(commonName, scientificName, category, description, approachable)
+        animalContainer.appendChild(userCard)
+        document.body.appendChild(animalContainer)
+    }
     
     hideSignIn() 
     retrieveAnimals()
