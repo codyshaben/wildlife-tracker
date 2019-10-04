406c100787372fbf5742497826f5fac0af96802a
github username <github email address>
2019-10-04 09:47:25 -0600

    half workinghome button

--- wildlifeTrackerFrontEnd/index.js
+++ wildlifeTrackerFrontEnd/index.js
@@ -69,9 +69,12 @@ document.addEventListener('DOMContentLoaded', () => {
     
     homeButton.addEventListener("click", event => {
         console.log(event)
+        categories.innerText = ' '
+        animalContainer.innerText = ''
+        userInfo.innerText = ''
         showHomePage()
-        hideMammals()
-        hideSignIn()
+        // hideMammals()
+        // hideSignIn()
     })
 
     reptilesButton.addEventListener('click', event => {
@@ -82,9 +85,6 @@ document.addEventListener('DOMContentLoaded', () => {
         allReptiles.forEach(reptile => {
             createCards(reptile)
         })
-        // const reptilesTitle = document.createElement('h2')
-        // reptilesTitle.innerText = "Reptile's"
-        // animalContainer.prepend(reptilesTitle)
     })
 
     birdsButton.addEventListener('click', event => {
@@ -95,9 +95,6 @@ document.addEventListener('DOMContentLoaded', () => {
         allBirds.forEach(bird => {
             createCards(bird)
         })
-        // const birdsTitle = document.createElement('h2')
-        // birdsTitle.innerText = "Bird's"
-        // animalContainer.prepend(birdsTitle)
     })
 
     mammalsButton.addEventListener('click', event => {
@@ -108,9 +105,6 @@ document.addEventListener('DOMContentLoaded', () => {
         allMammals.forEach(mammal => {
             createCards(mammal)
         })
-        // const mammalsTitle = document.createElement('h2')
-        // mammalsTitle.innerText = "Mammal's"
-        // animalContainer.prepend(mammalsTitle)
     })
 
     // functions
@@ -222,41 +216,7 @@ document.addEventListener('DOMContentLoaded', () => {
             document.body.appendChild(animalContainer)
     }
 
-    // function createUserCards(animal){
-    //     const userCard = document.createElement('div')
-    //     const commonName = document.createElement('h1') 
-    //     const scientificName = document.createElement('h4')
-    //     const category = document.createElement('h5')
-    //     const description = document.createElement('p')
-    //     const approachable = document.createElement('p')
-    //     const status = document.createElement('h6')
-    //     const image = document.createElement('img')
-    //     const removeAnimalButton = document.createElement('button')
-
-    //     removeAnimalButton.addEventListener('click',event => { 
-    //         const animalToRemove = event.target.parentNode
-    //         fetch(removeAnimalURL + animal.id, {
-    //             method: 'DELETE',
-    //         }).then(animalToRemove.remove())
-    //     })
-
-    //     userCard.className = "user-card"
-
-    //     removeAnimalButton.innerText = "Remove Animal"
-    //     commonName.innerText = animal.common_name
-    //     scientificName.innerText = 'Scientific Name: ' + animal.scientific_name
-    //     category.innerText = 'Category: ' + animal.category
-    //     description.innerText = animal.description
-    //     approachable.innerText = animal.approachable
-    //     status.innerText = animal.status
-    //     image.src = animal.image
-
-    //     userCard.appendChild(image)
-    //     userCard.append(commonName, scientificName, category, description, approachable, removeAnimalButton)
-    //     userAnimalContainer.appendChild(userCard)
-    //     userInfo.append(userAnimalContainer)
-    //     document.body.appendChild(userInfo)
-    // }
+   
 
     function createUserCards(animal){
        
